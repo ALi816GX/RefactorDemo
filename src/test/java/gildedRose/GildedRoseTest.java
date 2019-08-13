8967d9c2b9227a084ddf7db4834b017938f7ef2b
linguangxiong <854501715@qq.com>
2019-08-13 09:47:33 +0800

    Test(GildedRoseTest):test for the normal item case and cover all

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -1,6 +1,8 @@
 package gildedRose;
 
 
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created with IDEA
@@ -11,6 +13,40 @@ package gildedRose;
  * @description:
  */
 public class GildedRoseTest {
+
+
+    //-----normal-----//
+
+    @Test
+    public void should_return_item_given_a_normal_item_its_sellIn_is_5_quality_is_10() {
+
+        Item item = new Item("normal", 5, 10);
+        Item[] items = {item};
+
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+
+        Item result = gildedRose.items[0];
+
+        Assert.assertEquals("normal, 4, 9", result.toString());
+
+    }
+
+
+    @Test
+    public void should_return_item_given_a_normal_item_its_sellIn_is_0_quality_is_10(){
+
+        Item item = new Item("normal", 0, 10);
+        Item[] items = {item};
+
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+
+        Item result = gildedRose.items[0];
+
+        Assert.assertEquals("normal, -1, 8", result.toString());
+
+    }
 
 
 
